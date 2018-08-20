83490831c2b6dbf670a216ec2c58ce3eb9a97ffb
Thulani47 <thulaniinnocent47@gmail.com>
2018-08-20 16:55:37 +0200

    added xlsx.min.js and display excel data in html table format

--- Development/JavaScript/index.js
+++ Development/JavaScript/index.js
@@ -46,27 +46,21 @@ $(document).ready(()=>{
         uploadButton.click(()=>{
             if (file){
                var fileReader = new FileReader();
-               fileReader.onload = (e) =>{
-                   let data = e.target.result;
-                  
-                   var workbook = XLSX.read(data, {
-                    type: 'binary'
-                  });
-                  workbook.SheetNames.forEach(function(sheetName) {
-                    // Here is your object
-                    var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-                    var json_object = JSON.stringify(XL_row_object);
-                    console.log(json_object);
-            
-                  })
-            
-                };
+               
+               fileReader.onload = (e)=>{
+                let data = new Uint8Array(fileReader.result);
+
+                let wb = XLSX.read(data, {type: 'array'});
+
+                let htmlStr = XLSX.write(wb, {sheet: 'SalesOrders', type: 'binary', bookType:'html'});
+                $('#output').html(htmlStr)
+               }
             
                 fileReader.onerror = function(ex) {
                   console.log(ex);
                 };
             
-                fileReader.readAsBinaryString(file);
+                fileReader.readAsArrayBuffer(file)
             }else{
                 alert('no file selected')
             }
