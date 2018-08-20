10e3d69538715f7821d0464aa0e777524d8f7bc5
Thulani47 <thulaniinnocent47@gmail.com>
2018-08-20 03:41:42 +0200

    renamed files and added drag and drop feature

--- Development/JavaScript/index.js
+++ Development/JavaScript/index.js
@@ -1,21 +1,56 @@
 
-var s,t,u;
-s = 3;
-t = 7;
-u = s + t;
+$(document).ready(()=>{
 
-    const realFileBtn = document.getElementById("real-file");
-    const customBtn = document.getElementById("custom-button");
-    const customTxt = document.getElementById("custom-text");
+    if (window.File && window.FileReader && window.FileList && window.Blob){
 
-    customBtn.addEventListener("click", function(){
-            realFileBtn.click();
-    });
+        let customButton = $('#customButton');
+        let customText = $('#customText');
+        let uploadButton = $('#uploadText');
+         let dropZone = document.getElementById('dropZone');
+        let file;
 
-    realFileBtn.addEventListener("change", function(){
-        if(realFileBtn.value){
-            customTxt.innerHTML = realFileBtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
-        }else{
-            customTxt.innerHTML = "No File Chosen";
-        }
-    });
+
+        customButton.click(()=>{
+           $('#file').click();
+        });
+        $('#file').change(()=>{
+            if($('#file').val()){
+                console.log($('#file').val())
+                customText.html($('#file').val().match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1])
+            }else{
+                customText.html('No File Chosen')
+            }
+        });
+
+        dropZone.addEventListener('dragover', (e)=>{
+               e.stopPropagation();
+            e.preventDefault();
+            e.dataTransfer.dropEffect = 'copy' ;
+        },false);
+        dropZone.addEventListener('drop',(e)=>{
+                 e.stopPropagation();
+            e.preventDefault();
+            if (!e.dataTransfer.files[0].type==='xlsx'){
+                alert('we can only take xlsl files')
+            }
+            console.log(e.dataTransfer.files)
+        },false)
+
+        // $('#dropZone').on('dragover', (e)=>{
+        //     e.stopPropagation();
+        //     e.preventDefault();
+        //     e.dataTransfer.dropEffect = 'copy'
+        // }, false);
+        // $('#dropZone').on('drop', (e)=>{
+        //     e.stopPropagation();
+        //     e.preventDefault();
+        //     alert(e.dataTransfer.files[0].name)
+        // },false)
+
+
+
+    }else{
+        alert('The File APIs are not fully supported in this browser');
+    }
+});
+    
