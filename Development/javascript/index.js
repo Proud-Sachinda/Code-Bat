8d7c61cc8f0f3c5902c1f48b1e6154d179e0b9b0
Robel <robelberhnau89@gmail.com>
2018-08-23 22:31:36 +0200

    added export as pdf functionality

--- Development/javascript/index.js
+++ Development/javascript/index.js
@@ -1,71 +1,121 @@
+$(document).ready(() => {
 
-$(document).ready(()=>{
-
-    if (window.File && window.FileReader && window.FileList && window.Blob){
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
 
         let customButton = $('#customButton');
         let customText = $('#customText');
         let uploadButton = $('#uploadButton');
-         let dropZone = document.getElementById('dropZone');
-         let output = $('#output');
-
+        let dropZone = document.getElementById('dropZone');
+        let exportBtn = $('#exportBtn');
         let file;
 
 
 
-        customButton.click(()=>{
-           $('#file').click();
+
+        customButton.click(() => {
+            $('#file').click();
         });
-        $('#file').change((e)=>{
+        $('#file').change((e) => {
             file = e.target.files[0]
-            if($('#file').val()){
-               // console.log($('#file').val())
+            if ($('#file').val()) {
+                // console.log($('#file').val())
                 customText.html(file.name)
-            }else{
+            } else {
                 customText.html('No File Chosen')
             }
         });
 
-        dropZone.addEventListener('dragover', (e)=>{
-               e.stopPropagation();
+        dropZone.addEventListener('dragover', (e) => {
+            e.stopPropagation();
             e.preventDefault();
-            e.dataTransfer.dropEffect = 'copy' ;
-        },false);
-        dropZone.addEventListener('drop',(e)=>{
-                 e.stopPropagation();
+            e.dataTransfer.dropEffect = 'copy';
+        }, false);
+        dropZone.addEventListener('drop', (e) => {
+            e.stopPropagation();
             e.preventDefault();
             // if (!e.dataTransfer.files[0].type==='xlsx'){
             //     alert('we can only take xlsl files')
             // }
             file = e.dataTransfer.files[0];
             customText.html(file.name)
-            //console.log(e.dataTransfer.files[0])
-        },false)
+                //console.log(e.dataTransfer.files[0])
+        }, false)
 
         //uploading the file for processing
-        uploadButton.click(()=>{
-            if (file){
-               var fileReader = new FileReader();
+        uploadButton.click(() => {
+            if (file) {
+                var fileReader = new FileReader();
 
-               fileReader.onload = (e)=>{
-                let data = new Uint8Array(fileReader.result);
+                fileReader.onload = (e) => {
+                    let data = new Uint8Array(fileReader.result);
 
-                let wb = XLSX.read(data, {type: 'array'});
+                    let wb = XLSX.read(data, { type: 'array' });
 
-                let htmlStr = XLSX.write(wb, {sheet: 'SalesOrders', type: 'binary', bookType:'html'});
-                $('#output').html(htmlStr)
-               }
+                    let htmlStr = XLSX.write(wb, { sheet: 'SalesOrders', type: 'binary', bookType: 'html' });
+                    $('#output').html(htmlStr)
+                }
 
                 fileReader.onerror = function(ex) {
-                  console.log(ex);
+                    console.log(ex);
                 };
 
                 fileReader.readAsArrayBuffer(file)
-            }else{
+            } else {
                 alert('no file selected')
             }
+        });
+
+        let doc = new jsPDF();
+        let specialElementHandlers = {
+            "#editor": (element, renderer) => {
+                return true;
+            }
+
+        };
+
+        exportBtn.click(() => {
+            if (file) {
+                let select = document.getElementById('exportAs');
+                let selectedOption = select.options[select.selectedIndex].value;
+
+                switch (selectedOption) {
+                    case "pdf":
+                        doc.fromHTML($("#output").html(), 20, 20, {
+                            'width': 170,
+                            'elementHandlers': specialElementHandlers
+                        });
+                        doc.save(file.name + "pdf");
+                        break;
+                    case "png":
+                        alert("to save as png");
+                        break;
+                    case "pps":
+                        alert("to save as pps");
+                        break;
+
+                    default:
+                        break;
+                }
+            }
         })
-    }else{
+
+
+
+    } else {
         alert('The File APIs are not fully supported in this browser');
     }
 });
+
+
+
+// pdfButton.click(() => {
+//     var doc = new jsPDF();
+//     file = e.target.files[0]
+
+
+//     doc.froHTML($('#toUse').get(0), 20, 20, {
+//         'width': 500
+//     });
+//     doc.save("test.pdf");
+
+// });
