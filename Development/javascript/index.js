c488d84c9ecbad74a7a82a70acae5a424679bd64
Thwani Sithole <thulaniinnocent47@gmail.com>
2018-08-27 20:46:21 +0200

    added html2canvas.js and filesaver.js to convert contents of output div to png format and provide save option

--- Development/javascript/index.js
+++ Development/javascript/index.js
@@ -87,15 +87,15 @@ $(document).ready(() => {
                         doc.save(file.name + "pdf");
                         break;
                     case "png":
-                        domtoimage.toPng(theOutput)
-                            .then(function(dataUrl) {
-                                var img = new Image();
-                                img.src = dataUrl;
-                                document.body.appendChild(img);
-                            })
-                            .catch(function(error) {
-                                console.error('oops, something went wrong!', error);
-                            });
+                        let element = document.getElementById('output');
+                        html2canvas(element).then((canvas)=>{
+                        	let base64Image = canvas.toDataURL("image/png");
+                        	var block = base64Image.split(";");
+			    			var mimeType  = block[0].split(":")[1];
+			    			var realData = block[1].split(",")[1];
+			    			var canvasBlob = b64toBlob(realData, mimeType);
+                        	window.saveAs(canvasBlob, file.name+ "png");
+                        })
 
                     case "pps":
 
@@ -108,6 +108,40 @@ $(document).ready(() => {
                 }
             }
         })
+
+
+        /**
+			 * Convert a base64 string in a Blob according to the data and contentType.
+			 * 
+			 * @param b64Data {String} Pure base64 string without contentType
+			 * @param contentType {String} the content type of the file i.e (image/jpeg - image/png - text/plain)
+			 * @param sliceSize {Int} SliceSize to process the byteCharacters
+			 * @see http://stackoverflow.com/questions/16245767/creating-a-blob-from-a-base64-string-in-javascript
+			 * @return Blob
+		 */
+        function b64toBlob(b64Data, contentType, sliceSize) {
+        contentType = contentType || '';
+        sliceSize = sliceSize || 512;
+
+        var byteCharacters = atob(b64Data);
+        var byteArrays = [];
+
+        for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+            var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+            var byteNumbers = new Array(slice.length);
+            for (var i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+            }
+
+            var byteArray = new Uint8Array(byteNumbers);
+
+            byteArrays.push(byteArray);
+        }
+
+      var blob = new Blob(byteArrays, {type: contentType});
+      return blob;
+	}
 
 
 
