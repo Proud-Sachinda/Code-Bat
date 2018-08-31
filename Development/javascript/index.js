efc6ef014563a02f9dc652edba33d7bd89d7b66e
Robel <robelberhanu89@gmail.com>
2018-08-31 08:38:14 +0200

    eps button updated

--- Development/javascript/index.js
+++ Development/javascript/index.js
@@ -88,16 +88,16 @@ $(document).ready(() => {
                         break;
                     case "png":
                         let element = document.getElementById('output');
-                        html2canvas(element).then((canvas)=>{
-                        	let base64Image = canvas.toDataURL("image/png");
-                        	var block = base64Image.split(";");
-			    			var mimeType  = block[0].split(":")[1];
-			    			var realData = block[1].split(",")[1];
-			    			var canvasBlob = b64toBlob(realData, mimeType);
-                        	window.saveAs(canvasBlob, file.name+ "png");
+                        html2canvas(element).then((canvas) => {
+                            let base64Image = canvas.toDataURL("image/png");
+                            var block = base64Image.split(";");
+                            var mimeType = block[0].split(":")[1];
+                            var realData = block[1].split(",")[1];
+                            var canvasBlob = b64toBlob(realData, mimeType);
+                            window.saveAs(canvasBlob, file.name + "png");
                         })
 
-                    case "pps":
+                    case "eps":
 
                         break;
                         alert("to save as pps");
@@ -111,37 +111,37 @@ $(document).ready(() => {
 
 
         /**
-			 * Convert a base64 string in a Blob according to the data and contentType.
-			 * 
-			 * @param b64Data {String} Pure base64 string without contentType
-			 * @param contentType {String} the content type of the file i.e (image/jpeg - image/png - text/plain)
-			 * @param sliceSize {Int} SliceSize to process the byteCharacters
-			 * @see http://stackoverflow.com/questions/16245767/creating-a-blob-from-a-base64-string-in-javascript
-			 * @return Blob
-		 */
+         * Convert a base64 string in a Blob according to the data and contentType.
+         * 
+         * @param b64Data {String} Pure base64 string without contentType
+         * @param contentType {String} the content type of the file i.e (image/jpeg - image/png - text/plain)
+         * @param sliceSize {Int} SliceSize to process the byteCharacters
+         * @see http://stackoverflow.com/questions/16245767/creating-a-blob-from-a-base64-string-in-javascript
+         * @return Blob
+         */
         function b64toBlob(b64Data, contentType, sliceSize) {
-        contentType = contentType || '';
-        sliceSize = sliceSize || 512;
+            contentType = contentType || '';
+            sliceSize = sliceSize || 512;
 
-        var byteCharacters = atob(b64Data);
-        var byteArrays = [];
+            var byteCharacters = atob(b64Data);
+            var byteArrays = [];
 
-        for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-            var slice = byteCharacters.slice(offset, offset + sliceSize);
+            for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+                var slice = byteCharacters.slice(offset, offset + sliceSize);
 
-            var byteNumbers = new Array(slice.length);
-            for (var i = 0; i < slice.length; i++) {
-                byteNumbers[i] = slice.charCodeAt(i);
+                var byteNumbers = new Array(slice.length);
+                for (var i = 0; i < slice.length; i++) {
+                    byteNumbers[i] = slice.charCodeAt(i);
+                }
+
+                var byteArray = new Uint8Array(byteNumbers);
+
+                byteArrays.push(byteArray);
             }
 
-            var byteArray = new Uint8Array(byteNumbers);
-
-            byteArrays.push(byteArray);
+            var blob = new Blob(byteArrays, { type: contentType });
+            return blob;
         }
-
-      var blob = new Blob(byteArrays, {type: contentType});
-      return blob;
-	}
 
 
 
